93c56faa90395fb5365fa571f4bee4ae01daa564
unusep <unusep@gmail.com>
2016-09-10 13:21:33 +0800

    Add post model and controller template

---
+++ api/models/Post.js
@@ -0,0 +1,14 @@
+/**
+ * Post.js
+ *
+ * @description :: the post model represents a user's post
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+    }
+
+};
+
